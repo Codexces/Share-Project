1c1ccbc4635fcf105cf8f6b9ecbe257fd417738b
Bittu Shah <86102600+Codexces@users.noreply.github.com>
2023-11-27 21:46:10 +0530

    Done

---
+++ FinanceeZee/js/demo.js
@@ -0,0 +1,68 @@
+// 1 Carousel slider
+$('#walkthrough-slider, .artistry-slider, .blog_section').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+// 4 Carousel slider
+$('#testimonials, .blog_slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
+// 3 Carousel slider
+$('#consultancy-slider,#trainingsec').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
